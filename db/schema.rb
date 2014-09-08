9a7ca5763328cc7b4990b5a9d5fd9929a676ea7c
Tanner Steward & Andrew Akers <student@epicodus.com>
2014-09-08 12:14:33 -0700

    add migration for posts and adding images to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140908164715) do
+ActiveRecord::Schema.define(version: 20140908191204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.string   "description"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
